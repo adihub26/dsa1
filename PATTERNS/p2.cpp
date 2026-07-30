4b05072d17ff3cd92420e85e9e3fb5809a3ec92b
adihub26 <agpillai26@gmail.com>
2026-07-30 22:43:25 +0530

    p2 u

--- PATTERNS/p2.cpp
+++ PATTERNS/p2.cpp
@@ -1,23 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solution {
-public:
+
     
-    void pattern2(int N) {
-        // Loop for rows
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j <= i; j++) {
+void p2(int N) {    
+    for (int i = 0; i < N; i++) {
+         for (int j = 0; j <= i; j++) {
                 cout << "* ";
-            }
-            cout << endl;
         }
+        cout << endl;
     }
-};
+}
 
 int main() {
-    Solution sol;
-    // Define N
-    int N = 5;
-    sol.pattern2(N);
+    int N;
+    cin>>N;
+    p2(N);
     return 0;
 }
