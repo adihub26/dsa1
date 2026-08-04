86c97ab114988d5432be1b06cdf56a3ef861d9f5
adihub26 <agpillai26@gmail.com>
2026-08-04 22:24:30 +0530

    pair sum1

---
+++ two pointers/pair with target sum.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool twosum(vector<int>&nums, int target){
+    unordered_set<int>seen;
+    for(int num:nums){
+        int complement=target- num;
+        if(seen.find(complement) != seen.end()){
+            return true;
+        }
+    seen.insert(num);
+    }
+    return false;
+}
+int main() {
+
+    int n;
+    cout << "Enter array size: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    int target;
+    cout << "Enter target: ";
+    cin >> target;
+
+    if (twosum(nums, target))
+        cout << "Pair exists";
+    else
+        cout << "Pair does not exist";
+
+    return 0;
+}
