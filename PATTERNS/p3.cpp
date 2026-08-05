4aead0e497cce9a9d4e34ea9853bfe7ee9ce0e2d
adihub26 <agpillai26@gmail.com>
2026-08-05 23:55:01 +0530

    update p1 and p3

--- PATTERNS/p3.cpp
+++ PATTERNS/p3.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-void p1(int N) {    
-    for (int i = 0; i < N; i++) {  
-        for (int j = 0; j < N; j++) {
-            cout << "* ";
-        }
+ void p3(int N) {
         
+    for (int i = 1; i <= N; i++) {
+            
+        for (int j = 1; j <= i; j++) {
+                cout << j << " ";
+        }
         cout << endl;
     }
 }
@@ -14,6 +15,6 @@ int main() {
     int N;
     cout<<"Enter number:";
     cin>>N;
-    p1(N); 
+    p3(N); 
     return 0;
 }
