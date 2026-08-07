7e7a6bb2e65e69c14da7929b26310a2fba6c7bea
adihub26 <agpillai26@gmail.com>
2026-08-07 22:39:04 +0530

    p7

---
+++ PATTERNS/p7.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+void p7(int N) {
+    for (int i = 0; i < N; i++) {
+            
+        for (int j = 0; j < N - i - 1; j++) {
+                cout << " ";
+        }
+
+        for (int j = 0; j < 2 * i + 1; j++) {
+                cout << "*";
+        }
+        for (int j = 0; j < N - i - 1; j++) {
+                cout << " ";
+        }
+        cout << endl;
+    }
+}
+int main() {
+    int N;
+    cout<<"Enter number:";
+    cin>>N;
+    p7(N); 
+    return 0;
+}
