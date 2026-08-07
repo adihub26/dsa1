75502c81ad44b70f7ece5865d2f9c5eafd5f6207
adihub26 <agpillai26@gmail.com>
2026-08-07 22:48:07 +0530

    p8

---
+++ PATTERNS/p8.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void p8(int N) {
+        // Outer loop for rows
+    for (int i = 0; i < N; i++) {
+            
+            // Print leading spaces (increases with row number)
+        for (int j = 0; j < i; j++) {
+                cout << " ";
+        }
+
+            // Print stars (decreases with row number)
+            // Formula: total stars = 2*N - (2*i + 1)
+        for (int j = 0; j < 2 * N - (2 * i + 1); j++) {
+                cout << "*";
+        }
+
+            // Print trailing spaces (same as leading spaces)
+        for (int j = 0; j < i; j++) {
+                cout << " ";
+        }
+
+            // Move to next row
+        cout << endl;
+    }
+}
+int main() {
+    int N;
+    cout<<"Enter number:";
+    cin>>N;
+    p8(N); 
+    return 0;
+}
