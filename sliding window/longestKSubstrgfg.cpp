e47529760ffeb9e168d00c149f9ef266810e2061
adihub26 <agpillai26@gmail.com>
2026-08-12 16:46:29 +0530

    longestKSubstr

---
+++ sliding window/longestKSubstrgfg.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int longestKSubstr(string &s,int k){
+    vector<int> freq(256,0);
+    int low=0;
+    int maxlen=0;
+    int d=0;
+    for(int high=0;high<s.length();high++){
+        if(freq[s[high]]==0){
+            d++;
+        }
+        freq[s[high]]++;
+        while(d>k){
+            freq[s[low]]--;
+            if(freq[s[low]]==0){
+                d--;
+            }
+            low++;
+        }
+        maxlen=max(maxlen,high-low+1);
+    }
+    return maxlen;
+
+}
+int main() {
+    string s;
+    int k;
+
+    cout << "Enter string: ";
+    cin >> s;
+
+    cout << "Enter k: ";
+    cin >> k;
+
+    cout << "Longest substring length: " << longestKSubstr(s, k) << endl;
+
+    return 0;
+}
